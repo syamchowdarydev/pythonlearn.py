f54a468d6627251e23e8d0503969271eadea9065
syamchowdarydev <syamchowdarybalusu@gmail.com>
2025-06-04 00:08:17 +0530

    Create fundametal_oper.py

---
+++ fundametal_oper.py
@@ -0,0 +1,22 @@
+print("name: Syam kiran")
+a = 2
+b = 4
+num1 = a+b
+print(num1)
+num2 = a-b
+print(num2)
+num3 = (a+b)/2
+print(num3)
+num4 = a*b
+print(num4)
+
+Num1 = int(input("enter the 1st number: "))
+Num2 = int(input("enter the 2nd number: "))
+Num3 = Num1+Num2
+print(Num3)
+Num4 = Num1-Num2
+print(Num4)
+Num5 = (Num1+Num2)/2
+print(Num5)
+Num6 = Num1*Num2
+print(Num6)
