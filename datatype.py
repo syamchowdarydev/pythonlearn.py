2d4985a36ba6602b995e4e611504d80b69fe2dfc
syamchowdarydev <syamchowdarybalusu@gmail.com>
2025-06-08 06:43:06 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,29 @@
+a = 10
+print(type(a))	# Output: <class 'int'>
+
+b = 10.5
+print(type(b))	# Output: <class 'float'>
+
+c = 2 + 4j
+print(type(c))	# Output: <class 'complex'>
+
+d = True
+print(type(d)) # Output: <class 'bool'>
+
+e = None
+print(type(e))	# Output: <class 'NoneType'>
+
+f = "hello"
+print(type(f)) # Output: <class 'str'>
+
+g = [1, 2, 3]
+print(type(g)) # Output: <class 'list'>
+
+h = (1, 2, 3)
+print(type(h))	# Output: <class 'tuple'>
+
+i = {1, 2, 3}
+print(type(i))	# Output: <class 'set'>
+
+j = {10: 'GFG', 20: 'ID'}
+print(type(j))	# Output: <class 'dict'>
